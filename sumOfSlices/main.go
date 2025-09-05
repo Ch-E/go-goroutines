f7e04b6046b967103ce798f611ae7d915d8aeebe
Ch-E <charlesterhuysen303@gmail.com>
2025-09-05 08:23:05 +0200

    sumOfSlices

---
+++ sumOfSlices/main.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+
+/*
+Problem: Concurrent Sum of Slices
+
+1. Splits a slice of integers into 2 halves.
+2. Starts two goroutines, each computing the sum of one half.
+3. Collects the partial sums from the goroutines using channels.
+4. Adds them up to print the total sum.
+*/
+
+func main() {
+	nums := []int{1, 2, 3, 4, 5, 6}
+	chFirst := make(chan int)
+	chSecond := make(chan int)
+
+	go sumFirstHalf(nums, chFirst)
+	go sumSecondHalf(nums, chSecond)
+
+	finalSum := 0
+	finalSum = <-chFirst + <-chSecond
+
+	fmt.Printf("Sum of slices: %d", finalSum)
+
+	select {}
+}
+
+func sumFirstHalf(nums []int, chFirst chan int) {
+	sum := 0
+	for i := 0; i < len(nums)/2; i++ {
+		sum += nums[i]
+	}
+	chFirst <- sum
+}
+
+func sumSecondHalf(nums []int, chSecond chan int) {
+	sum := 0
+	for i := len(nums) / 2; i < len(nums); i++ {
+		sum += nums[i]
+	}
+	chSecond <- sum
+}
