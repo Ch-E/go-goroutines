26bde90d0041bc23a93bbf0c0d2147733363503c
Ch-E <charlesterhuysen303@gmail.com>
2025-09-09 17:21:09 +0200

    fileDownloader

---
+++ fileDownloader/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+/*
+Problem: Concurrent File Downloader (Simulation)
+
+You need to simulate downloading 3 files concurrently.
+
+1. You have a slice of file names:
+	files := []string{"file1.txt", "file2.txt", "file3.txt"}
+2. For each file, start a goroutine using wg.Go that:
+	Prints Downloading <filename>...
+	Sleeps for 1 second (to simulate download time).
+	Prints Finished <filename>.
+3. Make sure the program waits for all downloads to finish before exiting.
+*/
+
+func main() {
+	files := []string{"file1.txt", "file2.txt", "file3.txt"}
+	var wg sync.WaitGroup
+
+	for i := range files {
+		idx := i
+		wg.Go(func() {
+			downloadFile(files[idx])
+		})
+	}
+
+	wg.Wait()
+}
+
+func downloadFile(file string) {
+	fmt.Println("Downloading " + file + "...")
+	time.Sleep(1 * time.Second)
+	fmt.Printf("Finished downloading %s\n", file)
+}
