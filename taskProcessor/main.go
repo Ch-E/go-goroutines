077a82049d10c7462ae864ff1e7484427c122cd6
Ch-E <charlesterhuysen303@gmail.com>
2025-10-04 21:26:48 +0200

    Added random wait duration to processTask

---
+++ taskProcessor/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"math/rand"
+	"time"
+)
+
+/*
+Problem: Concurrent Task Processor
+
+You’re building a mini task scheduler.
+
+1. You have a list of 5 tasks (just strings like "Task 1", "Task 2", …).
+2. Launch a goroutine for each task that:
+	Waits a random time (≤ 2s).
+	Then sends "Task X done" into a channel.
+	In the main goroutine, use a select loop to:
+3. Collect results from the tasks.
+	Print them as they finish (in whatever order they complete).
+	Also have a timeout (say 3 seconds).
+	If the timeout hits before all tasks are done, print "Not all tasks finished" and exit.
+*/
+
+func main() {
+	tasks := []string{
+		"Task 1",
+		"Task 2",
+		"Task 3",
+		"Task 4",
+		"Task 5",
+	}
+
+}
+
+func processTask(task string) {
+	randomDuration := time.Duration(rand.Intn(2000)) * time.Millisecond
+	time.Sleep(randomDuration)
+}
